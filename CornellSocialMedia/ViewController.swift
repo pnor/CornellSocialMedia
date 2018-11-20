f4fba28499a5691bffb2dd129267fe3816a6079f
Phillip O <philliporeggio@dhcp-clara-2038.eduroam.cornell.edu>
2018-11-19 22:02:25 -0500

    Initial Commit

---
+++ CornellSocialMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CornellSocialMedia
+//
+//  Created by Phillip OReggio on 11/19/18.
+//  Copyright © 2018 CS1998FinalPGAV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
