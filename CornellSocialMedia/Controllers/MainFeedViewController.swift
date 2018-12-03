6400ed8f13cb98b45803a2a38ef548a1df57c699
Phillip O'Reggio <pnoreggio@gmail.com>
2018-12-02 23:56:29 -0500

    Fix Main Feed

--- CornellSocialMedia/Controllers/MainFeedViewController.swift
+++ CornellSocialMedia/Controllers/MainFeedViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Phillip OReggio on 11/22/18.
 //  Copyright © 2018 CS1998FinalPGAV. All rights reserved.
 //
-
 import UIKit
 import Hero
 import SnapKit
@@ -15,6 +14,10 @@ class MainFeedViewController: UIViewController, UICollectionViewDelegate, UIColl
     // Navigation Bar
     var search : UIBarButtonItem!
     var profile : UIBarButtonItem!
+    
+    // UI Elements
+    var postButton : UIButton!
+    var divider : UIView!
     
     // Main Display
     var messagesCollection : UICollectionView!
@@ -26,29 +29,30 @@ class MainFeedViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     // Data displayed
     var posts : [Post]?  // Full of dummy Posts rn until Networking Happens
-    var cellHeights : [CGFloat]? // All cell heights for each post
     let maxPosts = 30 // Loaded in memory
-    let maxLinesOfTextPost = 10
-    let maxLinesOfCaption = 2
+    let maxLinesOfTextPost = 0
+    let maxLinesOfCaption = 0
     
     // Padding and Sizing
     let padding : CGFloat = 20
     let charactersPerLine = 35 // an estimate
-    let lineSize = 24 // also an estimate
+    let lineSize = 22 // also an estimate
     var largestCellHeight : CGFloat = 0
     var smallestCellHeight : CGFloat = 0
     // Usual Message Heights
     let blankMessageHeight : CGFloat = 80
     let textMessageBaseHeight : CGFloat = 80
-    let imageCaptionMessageHeight : CGFloat = 350
-    let imageMessageHeight : CGFloat = 300
-
+    let imageMessageBaseHeight : CGFloat = 200
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: UI Elements
         navigationController?.setNavigationBarHidden(false, animated: true)
         navigationController?.navigationBar.isTranslucent = true
         navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 179/255, green: 27/255, blue: 27/255, alpha: 1.0)
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
         title = "Main Feed"
         
         search = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(goToSearch))
@@ -59,9 +63,8 @@ class MainFeedViewController: UIViewController, UICollectionViewDelegate, UIColl
         
         messagesLayout = UICollectionViewFlowLayout()
         messagesLayout.scrollDirection = .vertical
-        messagesLayout.minimumInteritemSpacing = 1000000
-        messagesLayout.minimumLineSpacing = 0
-        messagesLayout.estimatedItemSize = CGSize(width: UIScreen.main.bounds.width - CGFloat(padding * 2), height: 350)
+        messagesLayout.minimumLineSpacing = padding
+        messagesLayout.minimumInteritemSpacing = 10000
         
         messagesCollection = UICollectionView(frame: .zero, collectionViewLayout: messagesLayout)
         messagesCollection.delegate = self
@@ -71,61 +74,133 @@ class MainFeedViewController: UIViewController, UICollectionViewDelegate, UIColl
         messagesCollection.register(MainFeedCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         view.addSubview(messagesCollection)
         
+        // Post Button
+        postButton = UIButton()
+        postButton.setTitle("Post", for: .normal)
+        postButton.addTarget(self, action: #selector(createPost), for: .touchDown)
+        postButton.setTitleColor(.red, for: .normal)
+        postButton.layer.cornerRadius = 5
+        postButton.layer.masksToBounds = true
+        postButton.backgroundColor = .white
+        view.addSubview(postButton)
+        
+        divider = UIView()
+        divider.backgroundColor = .white
+        view.addSubview(divider)
+        
+        // Swiping
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+        
         // Refresh Control
         refreshControl = UIRefreshControl()
         refreshControl.tintColor = .white
-        messagesCollection.addSubview(refreshControl)
+        if #available(iOS 10.0, *) {
+            messagesCollection.refreshControl = refreshControl
+        } else {
+            messagesCollection.addSubview(refreshControl)
+        }
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-
+        
         // MARK: Animations
         hero.isEnabled = true
         view.hero.id = "backdrop"
-        view.applyGradient(with: [UIColor(hue: 0, saturation: 0.8, brightness: 1, alpha: 1), UIColor(hue: 0, saturation: 1, brightness: 1, alpha: 0.5)], gradient: .topLeftBottomRight)
+        view.applyGradient(with: [
+            UIColor(hue: 0, saturation: 1, brightness: 1, alpha: 0.3),
+            UIColor(hue: 0, saturation: 1, brightness: 1, alpha: 0.6)],
+                           gradient: GradientOrientation.topLeftBottomRight)
         view.backgroundColor = .white
         
         //TODO Get Post Data
-        refresh()
-        messagesCollection.reloadData()
+        posts = getPosts()
         
         setupConstraints()
     }
     
     // MARK: - UI Positioning
     func setupConstraints() {
+        postButton.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.leading.equalToSuperview().offset(padding)
+            make.width.equalTo(180)
+            make.height.equalTo(30)
+        }
+        
+        divider.snp.makeConstraints { (make) in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(postButton.snp.bottom).offset(10)
+            make.height.equalTo(3)
+        }
         messagesCollection.snp.makeConstraints { (make) in
-            make.edges.edges.equalToSuperview().inset(UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0))
+            make.top.equalTo(divider)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    // MARK: - Handling Swipes
+    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case .right:
+                goToSearch()
+            case .left:
+                goToProfile()
+            default:
+                print("Swiped in a unhandled direction")
+            }
         }
     }
     
-    // MARK: - Navigation Bar Button Methods and Refresh
+    // MARK: - Navigation Bar Button Methods and Refresh + Post Button
     @objc func goToProfile() {
         self.navigationController?.pushViewController(ProfileViewController(), animated: true)
     }
     
     @objc func goToSearch() {
-        let peopleSearch = SearchViewController()
-        navigationController?.pushViewController(peopleSearch, animated: true)
+        navigationController?.pushViewController(SearchViewController(), animated: true)
     }
     
+    @objc func createPost() {
+        var postViewController = PostViewController()
+        postViewController.allowsImagePosting = true
+        postViewController.fromComments = false
+        navigationController?.pushViewController(postViewController, animated: true)
+    }
+    
+    
     @objc func refresh() {
-        print("Refresh")
+        // Update all cell heights
+        let timer = Timer(timeInterval: 3.0, repeats: false, block: { (timer) in
+            DispatchQueue.main.async {
+                self.posts = self.getPosts()
+                self.messagesCollection.reloadData()
+            }
+            self.refreshControl.endRefreshing()
+        })
+        timer.fire()
+    }
+    
+    func getPosts() -> [Post] {
         // Get some posts:
         // Debug Vars
-        let useRandomGenerated = false
-        let generatedTypes : [Debugging.PostType] = [.short, .medium, .long, .image, .imageCaptionless]
+        let useRandomGenerated = true
+        let generatedTypes : [Debugging.PostType] = [.image, .short, .medium, .long]
         
-        posts = []
+        var curPosts : [Post] = []
         if useRandomGenerated { // random
             for _ in 1...maxPosts {
-                posts?.append(Debugging.getPostType(generatedTypes))
+                curPosts.append(Debugging.getPostType(generatedTypes))
             }
         } else { // pre made set
-            posts? = Debugging.getDefaultPosts()
+            curPosts = Debugging.getDefaultPosts()
         }
-        
-        // Update all cell heights
-        cellHeights = [CGFloat](repeating: 0, count: posts?.count ?? 0)
-        print(posts ?? "Nothing...😨")
+        return curPosts
     }
     
     // MARK: - Collection View Methods
@@ -144,10 +219,10 @@ class MainFeedViewController: UIViewController, UICollectionViewDelegate, UIColl
             cell = messagesCollection.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
             if let feedCell = cell as? MainFeedCollectionViewCell {
                 feedCell.clean()
-                feedCell.maxLinesOfTextPost = maxLinesOfTextPost
-                feedCell.maxLinesOfCaption = maxLinesOfCaption
                 feedCell.configure(with: post)
+                feedCell.hero.id = "Post\(indexPath.row)"
             }
+            cell.setNeedsUpdateConstraints()
             return cell
         }
         fatalError("No cell at that postition! IndexPath.row = \(indexPath.row)")
@@ -155,29 +230,19 @@ class MainFeedViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     // MARK: UICollectionViewDelegate
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("selected \(String(describing: posts?[indexPath.row])) Should go to another screen")
-        return
+        let commentViewController = CommentViewController()
+        commentViewController.setCell(with: posts?[indexPath.row] as! Post)
+        commentViewController.setAnimationData(with: indexPath)
+        navigationController?.pushViewController(commentViewController, animated: true)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        updateCellSizing(height: messagesLayout.estimatedItemSize.height, postAtIndex: indexPath.row)
-        return messagesLayout.estimatedItemSize
+        if let post = posts?[indexPath.row] {
+            return sizeForPost(post: post)
+        }
+        fatalError("no post for this position!")
     }
     
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        print("Height: \(cellHeights?[section] ?? 0)   nil? \(cellHeights == nil)")
-//        print("Section: \(section)")
-//        print("returns... \(padding - largestCellHeight - (cellHeights?[section] ?? 0))")
-//        return padding + largestCellHeight - (cellHeights?[section] ?? 0)
-//    }
-    
-    // MARK: Display Utility Methods
-    /// Updates the largest and smallest variables with passed in parameters. Also updates the list of all cell heights
-    func updateCellSizing(height: CGFloat, postAtIndex: Int) {
-        cellHeights?[postAtIndex] = height
-        smallestCellHeight = min(height, smallestCellHeight)
-        largestCellHeight = max(height, largestCellHeight)
-    }
     
     //MARK: - Display Utility Methods
     func sizeForPost(post: Post) -> CGSize {
